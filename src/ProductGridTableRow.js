2f744c85a75aa633ef1617284800ece70eff2da7
Victor Kareta <victorkareta@gmail.com>
2019-01-29 00:01:26 +0200

    Make product name a link to the editor page

--- src/ProductGridTableRow.js
+++ src/ProductGridTableRow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductGridTableRow = ({ product }) => (
   <tr>
     <td>
-      {product.name}
+      <Link to={'products/' + product.id}>{product.name}</Link>
     </td>
     <td>
       {product.price}
