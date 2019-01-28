10cd6a56157d09203b2711e3edc845190ee5835a
Victor Kareta <victorkareta@gmail.com>
2019-01-29 00:28:57 +0200

    Set up redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import ProductGrid from './ProductGrid';
 import {Switch} from "react-router-dom";
 import Route from "react-router-dom/es/Route";
 import ProductEditor from "./ProductEditor";
+import { Provider } from 'react-redux';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path='/' component={ProductGrid}/>
-        <Route path='/products/:id' component={ProductEditor}/>
-      </Switch>
+      <Provider store={store}>
+        <Switch>
+          <Route exact path='/' component={ProductGrid}/>
+          <Route path='/products/:id' component={ProductEditor}/>
+        </Switch>
+      </Provider>
     );
   }
 }
