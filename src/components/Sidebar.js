d6f817aacf266f3d791fb8569807d070ffb1b415
Victor Kareta <victorkareta@gmail.com>
2019-02-27 00:53:59 +0200

    Add font awesome

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTshirt } from '@fortawesome/free-solid-svg-icons';
+
 import './Sidebar.css';
 
 class Sidebar extends Component {
   render() {
     return (
       <Menu width={200}>
-        <Link to="/">Products</Link>
+        <Link to="/">
+          <FontAwesomeIcon icon={faTshirt} />
+          <span>Products</span>
+        </Link>
       </Menu>
     );
   }
